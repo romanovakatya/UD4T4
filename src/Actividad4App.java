90e84f99bf735b64e385fdc99890c3865685fd33
Katia <katerina.bogrom@gmail.com>
2021-07-14 17:06:25 +0200

    initial commit

---
+++ src/Actividad4App.java
@@ -0,0 +1,28 @@
+
+public class Actividad4App {
+
+	public static void main(String[] args) {
+		
+		// declaración de las variables,
+		int N = 1;
+		
+		//valor inicial,
+		System.out.println("Valor inicial de N = " + N);
+		
+		//Incrementar N en 77,
+		N = N + 77;
+		
+		System.out.println("N + 77 = " + N);
+		
+		//Decrementar N en 3,
+		N = N - 3;
+		
+		System.out.println("N - 3 = " + N);
+		
+		//Duplicar valor de N,
+		N = N * 2;
+		
+		System.out.println("N * 2 = " + N);
+
+	}
+}
